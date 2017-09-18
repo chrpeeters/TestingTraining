f85341d77f6e6432c758609a102eac50fe7ea3c0
Christophe Peeters <chrpeeters@gmail.com>
2017-09-18 16:38:43 +0200

    Creating projects

---
+++ Quotations.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quotations.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ogone")]
+[assembly: AssemblyProduct("Quotations.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright © Ogone 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2069a28-b289-47a3-b90a-8cae2f2315a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
